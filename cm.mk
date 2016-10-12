c88e94056c7e574975fe346bb8a1f4bf450fdc83
truongvip1999z <truongvo18@gmail.com>
2016-10-12 16:12:06 +0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 # Release name
-PRODUCT_RELEASE_NAME := HM2014011
+PRODUCT_RELEASE_NAME := A328
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/Xiaomi/HM2014011/device_HM2014011.mk)
+$(call inherit-product, device/Lenovo/A328/device_A328.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := HM2014011
-PRODUCT_NAME := cm_HM2014011
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := HM2014011
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_DEVICE := A328
+PRODUCT_NAME := cm_A328
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := A328
+PRODUCT_MANUFACTURER := Lenovo
 
 ##PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="mt6582-user 6.0 MRA58M 2280749 release-keys"
